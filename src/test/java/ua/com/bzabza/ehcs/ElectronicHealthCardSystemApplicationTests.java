2c1cfc42bcc0de4b5719fe1b84b2edeeb6a855e1
aelmod <aelmod@mail.ru>
2017-05-28 02:03:42 +0300

    heroku init

---
+++ src/test/java/ua/com/bzabza/ehcs/ElectronicHealthCardSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.com.bzabza.ehcs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElectronicHealthCardSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
